b2fed7d1f5febb321e9ff4988a7b470a342a75a0
cauld19 <50849710+cauld19@users.noreply.github.com>
2019-12-14 09:09:54 -0800

    first commit/ login page working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const  [ credentials, setCredentials ] = useState({
+    username: "Lambda School",
+    password: "i<3Lambd4"
+  })
+
+  const handleSubmit = e => {
+    console.log(credentials)
+    e.preventDefault();
+    axios
+        .post("http://localhost:5000/api/login", credentials)
+        .then(res => {
+            console.log(res);
+            localStorage.setItem("token", res.data.payload)
+            props.history.push("/BubblePage")
+        })
+        .catch(err =>
+            console.log(err)    
+        )
+}
+
+
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <div className="login-form-main">
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="username"
+                    value={credentials.username}
+                    onChange={e => setCredentials({username: e.target.value})}
+                />
+                <input
+                    type="text"
+                    name="password"
+                    value={credentials.password}
+                    onChange={e => setCredentials({password: e.target.value})}
+                />
+                <button>Login</button>
+            </form>
+      </div>
   );
 };
 
